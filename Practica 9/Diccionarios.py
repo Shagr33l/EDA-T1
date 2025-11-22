ce72ffe836f990a874df91c470bff550af9aba10
Shagr33l <fixcmie@gmail.com>
2025-11-21 21:18:18 -0600

    Create Diccionarios.py with element dictionaries
    Initialize dictionaries for elements and demonstrate usage.

---
+++ Practica 9/Diccionarios.py
@@ -0,0 +1,18 @@
+Elementos = {'Hidrogeno': 1, 'Helio': 2, 'Carbon': 6}
+print(Elementos)
+print(elementos['Hidrogeno'])
+Elementos ['Litio'] = 3 
+Elementos ['Nitrogeno'] = 8
+print(elementos)
+Elementos2 = {}
+Elementos2 = ['H'] = {'Name':'Hydrogen', 'Number':1, 'Weight':1.00794} 
+Elementos2 = ['He'] = {'Name':Helium, 'Numer':2, 'Weight':4.002602}
+print(Elementos2['H'])
+print(Elementos2['H']['Name'])
+print(Elementos2['H']['Number'])
+Elementos2['H']['Weight'] = 4.30
+print(Elementos2['H']['Weight'])
+Elementos2['H'].update({'gas noble':True})
+print(Elementos2['H'])
+print(Elementos2.items())
+print(Elementos2.keys())
