a31aeefe309081b4e8c0e2c257e4e5c3d51810fd
Shagr33l <fixcmie@gmail.com>
2025-11-21 20:29:20 -0600

    Enhance variables.py with new variables and prints
    Added multiple variable assignments and type checks.

---
+++ Practica 9/variables.py
@@ -0,0 +1,14 @@
+x = 10
+print(x) 
+cadena = "Hola mundo"
+print(cadena) 
+x = y = z = 10
+print(x,y,z)
+type(x)
+type(cadena)
+x = "Hola mundo"
+cadena = 10
+type(x)
+type(cadena)
+SEGUNDOS_POR_DIA = 60*60*24
+PI = 3.1416
