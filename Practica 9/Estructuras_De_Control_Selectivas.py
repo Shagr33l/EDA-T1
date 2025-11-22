5dfe2f29f9c293e79f6acd0b21d59ee1f35341ec
Shagr33l <fixcmie@gmail.com>
2025-11-21 21:46:23 -0600

    Add functions for number comparison and output
    Implement multiple functions for comparing numbers and printing results based on conditions.

---
+++ Practica 9/Estructuras_De_Control_Selectivas.py
@@ -0,0 +1,49 @@
+def ObtenerMayor(param1, param2):
+  if param1>param2:
+    print("{} es mayor que {}".format(param1, param2))
+
+x = y = Z = 3
+if x == y == z:
+  print(True)
+
+def ObtenerMayor2(Param1, param2):
+  if param1<param2:
+    return param2
+  else:
+    return param1
+
+def ObtenerMayor_Idioma(param1, param2):
+  valor = param2 if (param1<param2) else (return param1)
+  return valor
+
+def numeros(num):
+  if num == 1:
+    print("Tu numero es 1")
+  elif num == 2:
+    print("Tu numero es 2")
+  elif num == 3:
+    print("Tu numero es 3")
+  elif num == 4:
+    print("Tu numero es 4")
+  else:
+    print(No hay opcion)
+
+def Numeros_Idiom(num):
+  if num in (1, 2, 3, 4):
+    print("Tu numero es {}".format(num))
+  else:
+    print("{} no es una opcion valida.".format(num))
+
+def ObtenerMasGrande(a, b , c):
+  if a > b:
+    if a > c:
+      return a
+    else:
+      return c
+  else: 
+    if b > c:
+      return b
+    else: 
+      return c
+  
+  
