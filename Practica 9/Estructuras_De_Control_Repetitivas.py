1a7e79d11ba11715284db45a0b9dedd612cbe8a9
Shagr33l <fixcmie@gmail.com>
2025-11-22 09:33:41 -0600

    Add Cuenta and Factorial functions
    Implement Cuenta and Factorial functions in Estructuras_De_Control_Repetitivas.py

---
+++ Practica 9/Estructuras_De_Control_Repetitivas.py
@@ -0,0 +1,21 @@
+def Cuenta(Limite):
+  i = limite
+  while True: 
+    print(i)
+    i = i -1
+    if i == 0:
+      break
+
+def Factorial(n):
+  i = 2
+  tmp = 1
+  while i<n-1:
+    tmp = tmp * i
+    i = i + 1
+    return tmp
+  
+  print (Factorial(4))
+  print (factorial(6))  
+
+    
+  
