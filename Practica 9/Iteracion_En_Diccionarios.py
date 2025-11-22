02c28ff9e802cf356f374516d402b3b582de1b49
Shagr33l <fixcmie@gmail.com>
2025-11-22 09:45:54 -0600

    Create Iteracion_En_Diccionarios.py

---
+++ Practica 9/Iteracion_En_Diccionarios.py
@@ -0,0 +1,29 @@
+Elementos = {'Hidrogeno':1, 'Helio': 2, 'Carbono': 6 }
+
+for llave, valor in elñementos.items():
+  print(Llave "=" Valor)
+
+for llave in elementos.keys():
+  print(llave)
+
+for valor in elementos.values():
+  print(Valor)
+for idx, x in enumerate(elementos):
+  print("El indice es {} y el elemento {}".format(idx, x))
+
+def Cuenta_Idiom(limite):
+  for i in range(limite, 0, -1):
+    print(i)
+  else:
+    print("Cuenta finalizada")
+
+def Cuenta_Idiom2(limite):
+  for i in range(limite, 0, -1):
+    print(i)
+    if i == 3:
+      break
+    else:
+      print("CUENTA FINALIZADA")
+    
+    
+  
