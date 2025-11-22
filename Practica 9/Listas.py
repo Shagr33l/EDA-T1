155993f7b9a7eabf7b9805519a7793e4f102fc61
Shagr33l <fixcmie@gmail.com>
2025-11-21 20:47:08 -0600

    Add lists for days of the month and number names
    Initialize lists for days of the month and numbers with corresponding values, and print their contents.

---
+++ Practica 9/Listas.py
@@ -0,0 +1,17 @@
+Lista_Dias_Del_mes=[31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+print(Lista_Dias_Del_mes)
+print(Lista_Dias_Del_mes[0])
+print(Lista_Dias_Del_mes[6])
+print(Lista_Dias_Del_mes[11])
+
+Lista_Numeros=[['cero', 0],['uno', 1, 'UNO'],['dos', 2],['tres', 3],['cuatro', 4],['x', 5]]
+print(Lista_Numeros)
+print(Lista_Numeros[0])
+print(Lista_Numeros[1])
+print(Lista_Numeros[2][0])
+print(Lista_Numeros[2][1])
+print(Lista_Numeros[1][0])
+print(Lista_Numeros[1][1])
+print(Lista_Numeros[1][2])
+Lista_Numeros[5][0] = 'cinco'
+print(Lista_Numeros[5])
