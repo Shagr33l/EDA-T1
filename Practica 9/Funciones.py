20c6ad2d2679c3240287b87fbc8b95edea42e409
Shagr33l <fixcmie@gmail.com>
2025-11-21 21:25:17 -0600

    Add functions for name printing and power calculations
    Define multiple functions for printing names, calculating squares, and returning multiple powers of a number.

---
+++ Practica 9/Funciones.py
@@ -0,0 +1,17 @@
+def Imprime_Nombre(nombre):
+    print("Hola "+nombre)
+  Imprime_Nombre("JJ")
+def Cuadrado(x):
+    return x**2
+  x = 5
+print("El cuadrado de {} es {}".format(x, cuadrado(x)))
+def varios(x):
+  return x**2, x**3, x**4
+  val1, val2, val3 = varios(2)
+  print("{} {} {}".format(val1, val2, val3))
+  def Cuadrado_Default(x==3):
+    return x**2
+  cuadrado_default()
+  val4, _, val5 = varios(2)
+  print("{} {}".format(val4, val5))
+  
